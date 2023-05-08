981c45bfeb15a73db946dae1947107b3f77265ec
C1l1r <c1l1rua@gmail.com>
2023-05-09 00:46:15 +0200

    Updated to process new format of data

--- dataaggregation/aggregation.py
+++ dataaggregation/aggregation.py
@@ -42,22 +42,36 @@ class data_aggregation:
                 unpr_data = f.read().decode('utf-8')
         except urllib.error.URLError as e:
             print(e.reason)
+
+
+
+
         df = pd.read_json(unpr_data).drop(['value'], axis=1)
-        temp_df = pd.DataFrame(columns = ['address', 'price','address1', 'price1'])
+        temp_df = pd.DataFrame(columns = [])
         listofdictjson = json.loads(unpr_data)
+
 
         for i in range(df.shape[0]):
             x = pd.read_json(listofdictjson[i]['value'])
-            x['address1'] = x.address[1]
-            x['price1'] = x.price[1]
-            temp_df = pd.concat([temp_df, x.drop([1],axis = 0)], ignore_index=True)
+            x['reserve_0'] = x.r[0]
+            x['reserve_1'] = x.r[1]
+            x.drop('r', axis = 1, inplace = True)
+            x.drop(1, inplace=True)
+            temp_df = pd.concat([temp_df, x], ignore_index=True)
         df = pd.concat([df, temp_df], axis=1)
+
+
+
+
         df['time'] = pd.to_datetime(df['time'], unit='ms')
-        df['price'] = pd.to_numeric(df['price'], errors='coerce')
-        df['price1']  = pd.to_numeric(df['price1'], errors='coerce')
-        df = df.loc[df['price'].notna() & df['price1'].notna()]
+        df['reserve_0'] = pd.to_numeric(df['reserve_0'], errors='coerce')
+        df['reserve_1']  = pd.to_numeric(df['reserve_1'], errors='coerce')
+        df = df.loc[df['reserve_0'].notna() & df['reserve_1'].notna()]
         df.set_index('time', inplace = True)
-        fig = px.line(df, x = df.index, y = 'price')
+        df = df.sort_index(level='date')
+
+
+        fig = px.line(df, x = df.index, y = 'reserve_0')
         display(fig)
         self.data=df
 
@@ -67,10 +81,10 @@ class data_aggregation:
             indicators=list(self.indicators.keys())
 
         params = {
-            'close' : self.data.resample(time).last().dropna()['price'],
-            'open' : self.data.resample(time).first().dropna()['price'],
-            'low' : self.data.resample(time).min().dropna()['price'],
-            'high' : self.data.resample(time).max().dropna()['price']
+            'close' : self.data.resample(time).last().dropna()['reserve_0'],
+            'open' : self.data.resample(time).first().dropna()['reserve_0'],
+            'low' : self.data.resample(time).min().dropna()['reserve_0'],
+            'high' : self.data.resample(time).max().dropna()['reserve_0']
         }
 
         for i in indicators:
@@ -82,4 +96,3 @@ class data_aggregation:
 
     def get_indicator(self, name):
         return self.indicators[name]
-

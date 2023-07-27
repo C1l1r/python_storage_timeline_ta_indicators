2babb699b70f1e8234ebffec6e2fe7851e8aac5b
C1l1r <c1l1rua@gmail.com>
2023-07-27 21:10:57 +0200

    Some stuff changed

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+      long_description = f.read()
+
 setup(name='python_storage_timeline_ta_indicators',
       version='0.1',
+      description='Tool to calculate technical analysis indicators in a continuous time series data',
+      long_description = long_description,
       author='Oleksandr Namchuk',
       author_email='c1l1rua@gmail.com',
       url='https://github.com/C1l1r/python_storage_timeline_ta_indicators',
